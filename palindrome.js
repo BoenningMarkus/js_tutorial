d149667995b37c99f65a72297b3dccc21f056d3f
BoenningMarkus <markusbonning@gmail.com>
2020-02-08 21:56:08 +0100

    prototypes in js / etwas Wiederholung, functions etc... / möchte palindrome app verstehen.

--- palindrome.js
+++ palindrome.js
@@ -5,13 +5,29 @@ function reverse(string) {
 }
 
 // Defines a Phrase Object
-
 function Phrase(content) {
     this.content = content;
 
+    // Returns content processed for palindrome testing.
+    this.processedContent = function processedContent() {
+       return this.content.toLowerCase();
+    }
+
     // Returns true for palindrome, false otherwise.
     this.palindrome = function palindrome() {
-        let processedContent = this.content.toLowerCase();
-        return processedContent === reverse(processedContent);
+        return this.processedContent() === reverse(this.processedContent());
+    }   
+}   
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+    this.content = content;
+    this.translation = translation;
+
+        // Returns content processed for palindrome testing.
+        this.processedContent = function processedContent() {
+            return this.translation.toLowerCase();
+         } 
 }
-}
+
+TranslatedPhrase.prototype = new Phrase ();
